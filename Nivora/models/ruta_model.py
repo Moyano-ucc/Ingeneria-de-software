2cc71ac84b11903791b0197c8f974ab966ed228d
Santiago Moyano NIño <santiagomoyanonino@gmail.com>
2026-09-12 16:46:16 +0000

    Actualización visual y funcional según el documento de diseño de NIVORA

--- Nivora/models/ruta_model.py
+++ Nivora/models/ruta_model.py
@@ -1,27 +1,27 @@
 class RutaModel:
     @staticmethod
     def obtener_rutas():
-        # Módulos de aprendizaje definidos para Nivora[cite: 1]
+        # Módulos y formato de progreso estipulados en RF-03 de Nivora
         return [
             {
                 "id": 1,
                 "nombre": "Control de flujo",
-                "completado": 0,
-                "total": 2,
-                "estado": "En progreso"
+                "completado": 1,
+                "total": 6,
+                "progreso_texto": "1/6"
             },
             {
                 "id": 2,
                 "nombre": "Listas",
                 "completado": 0,
-                "total": 6,
-                "estado": "Pendiente"
+                "total": 2,
+                "progreso_texto": "0/2"
             },
             {
                 "id": 3,
                 "nombre": "Funciones",
                 "completado": 0,
                 "total": 5,
-                "estado": "Pendiente"
+                "progreso_texto": "0/5"
             }
         ]
