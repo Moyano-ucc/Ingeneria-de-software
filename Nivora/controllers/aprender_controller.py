232fa0955aa2fa43699ad704a8a57836041bb5a1
Santiago Moyano NIño <santiagomoyanonino@gmail.com>
2026-09-14 08:41:42 -0500

    Delete Nivora directory

--- Nivora/controllers/aprender_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from models.ruta_model import RutaModel
-
-aprender_bp = Blueprint('aprender', __name__)
-
-@aprender_bp.route('/aprender')
-def mostrar_aprender():
-    rutas = RutaModel.obtener_rutas()
-    return render_template('aprender.html', rutas=rutas)
--- Nivora/controllers/practica_controller.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-practica_bp = Blueprint('practica', __name__)
-
-@practica_bp.route('/practica')
-def mostrar_practica():
-    return render_template('practica.html')
