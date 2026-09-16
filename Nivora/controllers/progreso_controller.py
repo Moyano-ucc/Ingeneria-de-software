2d88faa835798f48786e92570f5e68277af57990
Pedro Pablo Galvis Rios <pedropablogalvis250@gmail.com>
2026-09-16 14:04:07 +0000

    Mejoras de interfaz y progreso de Nivora

--- Nivora/controllers/progreso_controller.py
+++ Nivora/controllers/progreso_controller.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, render_template, redirect, url_for, session
+from models.progreso_model import ProgresoModel
 
 progreso_bp = Blueprint('progreso', __name__)
-
 
 @progreso_bp.route('/progreso')
 def mostrar_progreso():
     if 'usuario' not in session:
         return redirect(url_for('auth.login'))
 
-    return render_template('progreso.html', usuario=session['usuario'])
+    progreso = ProgresoModel.obtener_progreso()
+
+    return render_template(
+        'progreso.html',
+        usuario=session['usuario'],
+        progreso=progreso
+    )
